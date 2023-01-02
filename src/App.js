8d85c84d722d307ef7dfc6da61c93472d333da8b
Debayan Kumar Ghosh <84261787+Debayan97531@users.noreply.github.com>
2023-01-02 21:44:00 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import "./styles.css";
+import axios from "axios";
+import { useState, useEffect } from "react";
+
+export default function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
+      )
+      .then((resp) => {
+        setData(resp.data);
+        console.log(data);
+      })
+      .catch((e) => {});
+  }, []);
+  return (
+    <div
+      className="App"
+      style={{
+        minHeight: "100vh",
+        width: "100%",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center"
+      }}
+    >
+      <div className="prac-main">
+        {data.map((elem) => (
+          <div className="card">
+            <img src={elem.image} alt="dddfgd" />
+            <div className="card-content">
+              <h3>{elem.name}</h3>
+              <h3>{elem.symbol}</h3>
+              <h3>{elem.current_price}</h3>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
